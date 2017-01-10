8bc6c72b8d34266bcfa0c5bbcf0a0f4dfaaa50c9
viniabrahao <viniabrahao@users.noreply.github.com>
2017-01-10 20:07:02 -0200

    Add files via upload

---
+++ user.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Usuario</title>
+	</head>
+	<body>
+	<form method="post" action="cadastrar-enquete.php">
+<select name="time">
+<option value="bota">Botafogo</option>
+<option value="fla">Flamengo</option>
+<option value="flu">Fluminense</option>
+<option value="vasco">Vasco da Gama</option>
+</select>
+<input type="submit" value="Enviar" />
+<input type="reset" value="Cancelar" />
+</form>
+
+	</body>
+</html>
